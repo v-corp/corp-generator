e14641cb129dc567647bee651e4785c00b0b48e2
Samuel Garces <sagarma_94@hotmail.com>
2019-09-23 21:39:51 +0200

    Include windows build

--- bin/extract-info.ps1
+++ bin/extract-info.ps1
@@ -3,12 +3,13 @@ $hostname = hostname
 mkdir $hostname
 $Usuario = [Environment]::UserName
 # Configuracion de red
-Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Select-Object Description, SettingID, MACAddress, DNSDomain, DNSHostName, Index, InterfaceIndex, IPAddress | ConvertTo-Json > $hostname'\net.json'
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Select-Object Description, SettingID, MACAddress, DNSDomain, DNSHostName, Index, InterfaceIndex, IPAddress | ConvertTo-Json | Out-File -Encoding UTF8 $hostname'\net.json'
 # Discos instalados
-Get-WmiObject -Class Win32_LogicalDisk | Select-Object DeviceID, DriveType, FreeSpace, Size, VolumeName | ConvertTo-Json > $hostname'\disk.json'
+Get-WmiObject -Class Win32_LogicalDisk | Select-Object DeviceID, DriveType, FreeSpace, Size, VolumeName | ConvertTo-Json | Out-File -Encoding UTF8 $hostname'\disk.json'
 # Cuentas de usuario en el equipo, tanto locales como de dominio
-Get-WmiObject -ComputerName $hostname -Class Win32_UserAccount | Select-Object LocalAccount,AccountType,Name, PSComputerName, Description,SID, Lockout, PasswordChangeable, PasswordExpires, PasswordRequired  | ConvertTo-Json > $hostname'\accounts.json'
+Get-WmiObject -ComputerName $hostname -Class Win32_UserAccount | Select-Object LocalAccount,AccountType,Name, PSComputerName, Description,SID, Lockout, PasswordChangeable, PasswordExpires, PasswordRequired  | ConvertTo-Json | Out-File -Encoding UTF8 $hostname'\accounts.json'
 # Estructura de directorios de l carpeta home del usuario
 tree $HOME /f > $hostname/$Usuario'-tree.log'
 # Información de la BIOS
-Get-WmiObject Win32_Bios | ConvertTo-Json > $hostname/'bios.json'
+Get-WmiObject Win32_Bios | ConvertTo-Json | Out-File -Encoding UTF8 $hostname/'bios.json'
+[System.Environment]::OSVersion.Version | ConvertTo-JSON | Out-File -Encoding UTF8 $hostname'\version.json'
