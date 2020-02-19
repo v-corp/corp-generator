7acbb321712b3f4b41d0318aa0e1a52b5b5ce294
Samuel <sagarma_94@hotmail.com>
2020-02-20 00:02:17 +0100

    Use custom MACs from real vendors

---
+++ lib/mac_vendors.py
@@ -0,0 +1,33 @@
+import os
+import random
+
+def get_mac_list():
+    mac_list = []
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'mac-vendor.txt'),'r') as mac_file:
+        for line in mac_file: 
+            mac_list.append({'mac' : line[0:6].strip(), 'vendor' : line[6:].strip()})
+    return mac_list
+"""
+
+"""
+def search_wildcard_vendor(mac_list, vendor="*"):
+    if(vendor.startswith("*") and vendor.endswith("*")):
+        vendor = vendor[1:-1]
+        for vendor_mac in mac_list:
+            if(vendor in vendor_mac['vendor']):
+                return vendor_mac
+    elif(vendor.startswith("*")):
+        vendor = vendor[1:]
+        for vendor_mac in mac_list:
+            if(vendor_mac['vendor'].endswith(vendor)):
+                return vendor_mac
+    elif(vendor.endswith("*")):
+        vendor = vendor[:-1]
+        for vendor_mac in mac_list:
+            if(vendor_mac['vendor'].startswith(vendor)):
+                return vendor_mac
+    else:
+        for vendor_mac in mac_list:
+            if(vendor in vendor_mac['vendor']):
+                return vendor_mac
+    return mac_list[random.randint(0,len(mac_list))]
